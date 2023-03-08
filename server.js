e2cd17f0f466425b79a12fba65f4b14f95e22991
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-07 19:10:57 -0800

    more updates to server js, almost done

--- server.js
+++ server.js
@@ -52,5 +52,199 @@ function mainPrompt() {
             deleteDept();
         }
 });
+
+}
+function viewAllDept() {
+    let query = "SELECT departments.id, departments.dept_name FROM departments";
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+
+        console.table(result);
+        mainPrompt();
+
+    });
+};
+
+function viewAllRoles() {
+    let query = `SELECT roles.id, roles.employee_title AS title, departments.dept_name AS department, roles.salary'
+
+    FROM roles 
+    LEFT JOIN departments ON roles.department_id = department_id`;
+
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+
+        console.table(result);
+        mainPrompt();
+    });
+};
+function viewAllEmployees() {
+    let query = `SELECT
+    employees.id,
+    employees.first_name,
+    employees.last_name,
+    roles.employee_title AS title,
+    department.dept_name AS department,
+    roles.salary,
+    CONCAT(e2.first_name,'', e2.last_name) AS manager
+    
+    FROM employees
+
+    LEFT JOIN employees e2 ON e2.id = employees.manger_id
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id`;
+
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        mainPrompt();
+    })
 }
 
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'newDept',
+            message: 'What is the name of the department?'
+        }
+    ])
+    .then((response) => {
+        let query = "INSERT INTO departments SET?";
+        connection.query(query, {dept_name: response.newDept}, (err, result) => {
+            if (err) throw err;
+            console.log("New department added.");
+            mainPrompt();
+        })
+    });
+};
+function addRole() {
+    let query = "SELECT * FROM departments";
+
+    connection.query(query, function (err, result) {
+        let deptArry = [];
+        if (err) throw err;
+        for (let i =0; i< result.length; i++) {
+            deptArry.push(result[i].dept_name);
+        }
+
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'newRoleTitle',
+                message: 'Enter the new role title here'
+            },
+            {
+                type: 'input',
+                name: 'newSalary',
+                message: 'Please enter the correct salary for the role.'
+            },
+            {
+                type: 'list',
+                name: 'roleDept',
+                message: 'Please select the correct dept for this role',
+                choices: deptArry
+            }
+        ])
+        .then((response) => {
+            let query = "SELECT * FROM departments WHERE ?"; 
+
+            connection.query(query, {dept_name: response.roleDept}, function (err, result) {
+                if (err) throw err;
+                let query2 = "INSERT INTO roles SET ?";
+
+                connection.query(query2, {employee_title: response.newRoleTitle, salary: response.newSalary, department_id: result[0].id }, function (err, result) {
+                    if (err) throw err,
+                    console.log("New Role has been added.");
+                    mainPrompt();
+                });
+            });
+        });
+    });
+};
+
+
+function addEmployee() {
+    
+    let query = `SELECT * FROM roles`;
+
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+        
+        let titlesArry = [];
+        for (let i = 0; i < result.length; i++) {
+            titlesArry.push(result[i].employee_title);
+        }
+        function titleSet(arr) {
+            return [...new Set(arr)];
+        }
+
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'Please enter the first name.'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'Please enter the last name.'
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Please select the employee\'s title.',
+                choices: titleSet(titlesArry)
+            }
+            
+        ])
+        .then((response) => {
+            let query = `SELECT * FROM roles WHERE ?`;
+
+            connection.query(query, {employee_title: response.role}, (err, result) => {
+                if (err) throw err;
+
+                let query2 = `INSERT INTO employees SET ?`
+
+                connection.query(query2, {first_name: response.firstName, last_name: response.lastName, role_id: result[0].id}, (err, result) => {
+                    if (err) throw err;
+                    
+                    //process.exit(0);
+                    //console.log(result.insertId);
+                    addManager(result.insertId);
+                })
+                
+            });
+        });
+    });  
+};
+
+function addManager(employeeId) {
+    let query = `SELECT * FROM employees`;
+    
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+        let managerArry = ["none"];
+        for (let i = 0; i < result.length; i++) {
+            managerArry.push(result[i].first_name + " " + result[i].last_name);
+            //console.log(managerArry);
+        }
+
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'manager',
+                message: 'Please select this employee\'s manager',
+                choices: managerArry
+            }
+        ])
+        .then((response) => {
+            let query = `Select * FROM employees WHERE ?`
+
+        ]);
+    });
+}
